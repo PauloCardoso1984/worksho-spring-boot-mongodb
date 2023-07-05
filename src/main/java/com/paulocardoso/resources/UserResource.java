cbffbc8feff89b5887874f3936bf2f416f897b04
Paulinho <paulocardosoassessor@gmail.com>
2023-07-05 14:42:45 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/paulocardoso/resources/UserResource.java
+++ src/main/java/com/paulocardoso/resources/UserResource.java
@@ -1,30 +1,28 @@
 package com.paulocardoso.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.paulocardoso.domain.User;
+import com.paulocardoso.services.UserService;
 
 // RECURSO REST COM ANOTAÇÃO
 @RestController
 
 // CAMINHO DO ENDPOINT
-@RequestMapping (value= "/users")
+@RequestMapping(value= "/users")
 public class UserResource {
 
-	@RequestMapping(method= RequestMethod.GET)
+	@Autowired
+	private UserService service; 
+	
+	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity< List<User>> findAll() {
-		User davi = new User("1", "Davi", "davi@maria");
-		User paulo = new User("2", "Paulo", "paulo@gmail");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(davi, paulo));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
 }
---
+++ src/main/java/com/paulocardoso/services/UserService.java
@@ -0,0 +1,19 @@
+package com.paulocardoso.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.paulocardoso.domain.User;
+import com.paulocardoso.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
+ 
